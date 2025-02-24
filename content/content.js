60b19b7ed07c9262ecd6f9ab709fd800e6fc2031
= <=>
2025-02-24 21:46:37 +0200

    feat (popup box/ui): implement a popupbox to show the user tranlsation/explaination resualts

--- content/content.js
+++ content/content.js
@@ -1,8 +1,32 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "translate") {
-    console.log(`message from background: Translate`);
-  }
-  if (request.action === "explain") {
-    console.log(`message from background: Translate`);
-  }
+// setup container elements
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  setUpcontainer();
+  const message = await fetchContent();
+  updateText(message);
 });
+
+async function fetchContent() {
+  await chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    const data = request.data;
+    return data;
+  });
+}
+
+function setUpcontainer() {
+  const body = document.body;
+  const container = document.createElement("div");
+  const box = document.createElement("div");
+
+  container.classList.add("container");
+  box.classList.add("container__box");
+
+  container.append(box);
+  body.append(container);
+}
+
+function updateText(content) {}
