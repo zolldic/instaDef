aed0cafc0fbb997d6c22385d392c5a460c992d65
= <=>
2025-02-25 21:25:04 +0200

    feat (modal ui): implement a modal template in a jsx-like componenet

--- content/content.js
+++ content/content.js
@@ -1,32 +1,26 @@
-// setup container elements
 
-document.addEventListener("DOMContentLoaded", async (e) => {
-  setUpcontainer();
-  const message = await fetchContent();
-  updateText(message);
-});
 
-async function fetchContent() {
-  await chrome.runtime.onMessage.addListener(function (
-    request,
-    sender,
-    sendResponse
-  ) {
-    const data = request.data;
-    return data;
-  });
-}
 
-function setUpcontainer() {
-  const body = document.body;
-  const container = document.createElement("div");
-  const box = document.createElement("div");
+// modal template
+const createModal = (content) => {
+  const template = `
+    <div id="customModal" class="modal">
+      <div class="modal__content">
+        <div class="modal__content--body">
+          <p id="modalBody">${content}</p>
+        </div>
+        <div class="modal__content--footer">
+          <span id="close">&times;</span>
+        </div>
+      </div>
+    </div>
+`;
 
-  container.classList.add("container");
-  box.classList.add("container__box");
+  return document.createRange().createContextualFragment(template);
+};
 
-  container.append(box);
-  body.append(container);
-}
 
-function updateText(content) {}
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {})
+
+
+
