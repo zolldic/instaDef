c2d36665d548027aa74868e1868d80ac8e302d13
= <=>
2025-02-26 13:05:45 +0200

    feat (modal ui): handle showing the data, closing and opening the modal in the content script

--- content/content.js
+++ content/content.js
@@ -1,11 +1,16 @@
 // modal
-const createModal = (content) => {
+const createModal = () => {
   // explain modal
+
+  /**
+   * this function should inject templates into the dom
+   * based on the actions (translate, explain)
+   * inside model__content--body, the response from the AI
+   */
   const explainTemplate = `
     <div id="customModal" class="modal">
       <div class="modal__content">
-        <div class="modal__content--body">
-          <p id="modalBody">${content}</p>
+        <div class="modal__content--body" id="modalBody">
         </div>
         <div class="modal__content--footer">
           <span id="close">&times;</span>
@@ -17,17 +22,36 @@ const createModal = (content) => {
   return document.createRange().createContextualFragment(explainTemplate);
 };
 
+// when the webpage dom load
+// add the modal to the page
+document.body.onload = () => {
+  const modal = createModal();
+  document.body.append(modal);
+};
+
 // listen for message from background
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // create the modal
-  const modal = createModal(request.data);
-  // add the modal to the DOM
-  document.body.append(modal);
-  // open the modal
-  document.getElementById("customModal").style.display = "flex"; // Show modal
+  // add the data to the modal
+  document.getElementById("modalBody").append(request.data);
 
-  // close the modal
+  // open the modal
+  open();
+
+  // close the modal from the X button
   document.getElementById("close").addEventListener("click", () => {
-    document.getElementById("customModal").style.display = "none"; // Hide modal
+    close();
+  });
+
+  // close the modal when click outside
+  document.getElementById("customModal").addEventListener("click", () => {
+    close();
   });
 });
+
+function open() {
+  document.getElementById("customModal").style.display = "flex"; // Show modal
+}
+
+function close() {
+  document.getElementById("customModal").style.display = "none"; // Hide modal
+}
