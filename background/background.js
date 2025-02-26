e8050ee23adc68eddb04de0f71919f97c03421f8
= <=>
2025-02-26 02:02:33 +0200

    feat: implement getExplaination function

--- background/background.js
+++ background/background.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import "dotenv/config";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 chrome.runtime.onInstalled.addListener(async () => {
   // reload the extention automatically.
@@ -108,12 +108,27 @@ async function getTranslation(selection) {
   }
 }
 
+/**
+ * Provides an explanation for the given text selection by breaking it down into simpler parts.
+ *
+ * @async
+ * @function getExplaination * @param {string} selection - The text to be explained. * @param {string} selection - The text to be translated.
+ * @returns {Promise<string|null>} - The translated text or null if an error occurs.
+ * @throws Will throw an error if the translation request fails.
+ */
+
 async function getExplaination(selection) {
   try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const prompt = `Break down (${selection}) into smaller, easier-to-understand parts. Use analogies and real-life examples to simplify the concept and make it more relatable.`;
+    const res = await model.generateContent(prompt);
+    return res.response.text();
   } catch (error) {
     console.error(error);
   }
 }
+
 /**
 async function saveToStorage(key , value): {
     if (value || key) {
